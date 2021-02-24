bad04fc79090f47eb283d9d5295cf6336237177c
happens-nika <happens.nika@gmail.com>
2021-02-24 20:02:09 +0300

    making it work with back-end
    Changes necessary for front-end part to work with back-end (located in repo fullstackopen-part3)

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -2,18 +2,25 @@
 import axios from "axios";
 const baseUrl = "/api/persons";
 
+// Get all persons from phonebook db
 const getAll = () => {
   const request = axios.get(baseUrl);
   return request.then((response) => response.data);
 };
+
+// Add person to phonebook
 const create = (newObject) => {
   const request = axios.post(baseUrl, newObject);
   return request.then((response) => response.data);
 };
+
+// Delete person from phonebook
 const remove = (id) => {
   const request = axios.delete(`${baseUrl}/${id}`);
   return request.then((response) => response.data);
 };
+
+//Update person's number
 const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject);
   return request.then((response) => response.data);
